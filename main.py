6ad8ac3fbfce33a04553caa11dadbe0fa939a9f0
Thomas Arevalo <andreycasadiego05@gmail.com>
2026-04-30 15:49:28 -0500

    creacion de menu en main

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import a
-import modificar 
-import borrar
+
+print("Gestion de servicios fotografico")
+print("1.agregar paquete de fotografia \n 2.Editar servicio \n 3.Eliminar servicio \n 4.Salir ") 
+
+while True:
+    opcion = int(print("elegir opcion"))
+    if opcion == 1 :
+
+    elif opcion == 2:
+
+    elif opcion == 3:
+
+    elif opcion == 4:
+
