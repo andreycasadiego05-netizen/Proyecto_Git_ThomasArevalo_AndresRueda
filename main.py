e7eb54458260b7052068d06fc132c285358c00c6
Thomas Arevalo <andreycasadiego05@gmail.com>
2026-04-30 16:40:44 -0500

    merge tmb la opcion de añadir los servicios

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import añadir
-print("Gestion de servicios fotografico")
-print("1.agregar paquete de fotografia \n 2.Editar servicio \n 3.Eliminar servicio \n 4.Salir ") 
+from añadir import añadir_servicio
+
 
 while True:
-    opcion = int(print("elegir opcion"))
+    print("Gestion de servicios fotografico")
+    print("1.agregar paquete de fotografia \n 2.Editar servicio \n 3.Eliminar servicio \n 4.Salir ") 
+    opcion = int(input("elegir opcion: "))
     if opcion == 1 :
         añadir_servicio()
     elif opcion == 2:
-
+        añadir_servicio()
     elif opcion == 3:
-
+        añadir_servicio()
     elif opcion == 4:
-
+        añadir_servicio()
