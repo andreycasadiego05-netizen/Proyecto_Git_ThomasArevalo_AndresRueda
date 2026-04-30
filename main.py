80e03939170cf2431e4738669765eecdd5ca4975
Thomas Arevalo <andreycasadiego05@gmail.com>
2026-04-30 16:17:12 -0500

    agregado

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
+import añadir
 print("Gestion de servicios fotografico")
 print("1.agregar paquete de fotografia \n 2.Editar servicio \n 3.Eliminar servicio \n 4.Salir ") 
 
 while True:
     opcion = int(print("elegir opcion"))
     if opcion == 1 :
-
+        añadir_servicio()
     elif opcion == 2:
 
     elif opcion == 3:
